05d40bb7dac3b03327b785d7c36de5f7a444ad25
gunterstruck <224373224+gunterstruck@users.noreply.github.com>
2025-10-08 06:50:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Version bei Änderungen erhöhen, damit Caches sauber erneuert werden
-const CACHE_NAME = 'sprach-tagebuch-v5-5';
+const CACHE_NAME = 'sprach-tagebuch-v5-6';
 //const CACHE_NAME = 'sprach-tagebuch-v5-3';
 //const CACHE_NAME = 'sprach-tagebuch-v5';
 const ASSETS_TO_CACHE = [
